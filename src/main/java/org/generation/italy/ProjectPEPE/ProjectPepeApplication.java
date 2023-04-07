cfffd171111d196fa6197458970fd62bd267543c
RcoRed <notpurple.red@gmail.com>
2023-04-07 17:08:22 +0200

    Inizia ProjectPEPE

---
+++ src/main/java/org/generation/italy/ProjectPEPE/ProjectPepeApplication.java
@@ -0,0 +1,13 @@
+package org.generation.italy.ProjectPEPE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPepeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPepeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/generation/italy/ProjectPEPE/ProjectPepeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.italy.ProjectPEPE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPepeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
