d823af0fe6908b082b0135a800cee81f516a1142
MagrelliJoe <magrelli1988@gmail.com>
2023-04-12 18:30:27 +0200

    first

--- src/main/java/org/generation/italy/ProjectPEPE/ProjectPepeApplication.java
+++ src/main/java/org/generation/italy/ProjectPEPE/ProjectPepeApplication.java
@@ -1,5 +1,4 @@
 package org.generation.italy.ProjectPEPE;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectPepeApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ProjectPepeApplication.class, args);
 	}
 
